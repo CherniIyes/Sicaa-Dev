d2009ee5b31f935fa28808ea85c6239d4860960d
CherniIyes <cherniiyes@gmail.com>
2024-05-22 15:40:45 +0100

    final

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
         <div className="md:flex md:justify-between">
           <div className="mb-6 md:mb-0">
             <a href="/" className="flex items-center">
-              <span className="self-center text-3xl font-bold whitespace-nowrap text-black-500">SicaaDev</span>
+              <span className="self-center text-3xl font-bold whitespace-nowrap text-black-500">SiccaDev</span>
             </a>
           </div>
           <div className="grid grid-cols-2 gap-8 sm:gap-6 sm:grid-cols-3">
@@ -56,7 +56,7 @@ const Footer = () => {
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto lg:my-8" />
         <div className="sm:flex sm:items-center sm:justify-between">
-          <div className="text-sm text-gray-500 sm:text-center">© 2024 <a href="/" className="hover:underline">SicaaDev</a>. All Rights Reserved.</div>
+          <div className="text-sm text-gray-500 sm:text-center">© 2024 <a href="/" className="hover:underline">SiccaDev</a>. All Rights Reserved.</div>
           <div className="flex mt-4 sm:justify-center sm:mt-0 space-x-6">
             <a href="https://www.facebook.com/profile.php?id=61558877354559" className="text-gray-800 hover:text-black-500 ease-in-out duration-300">
               <FaFacebook size={24} />

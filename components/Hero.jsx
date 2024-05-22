b749d15ff415b76fbec0167eae4bd6cd5a0f5fb1
CherniIyes <cherniiyes@gmail.com>
2024-05-22 11:03:12 +0100

    nva-touches

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -13,13 +13,10 @@ const Hero = ({ heading, message }) => {
                   {/* overlay */}
                   <div className='absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2]' />
                   {/* overlay */}
-
                   <div className='p-5 text-white z-[2] '>
                         <h2 className='text-5xl font-bold'>{heading}</h2>
                         <p className='py-5 text-xl'>{message}</p>
                         <button className='px-8 py-2 border hover:cursor-default' >placeholder button</button>
-
-                        
                   </div>
             </div>
       )
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -12,6 +12,7 @@ import { Link as ScrollLink, animateScroll as scroll } from 'react-scroll';
 const links = [
     { url: "Hero", title: "Home" },
     { url: "aboutUS", title: "About" },
+    { url: "aboutUS", title: "Skills" },
     { url: "projects", title: "Projects" },
     { url: "Certificate", title: "Certificate" },
     { url: "contact", title: "Contact" },
@@ -39,20 +40,23 @@ const Navbar = () => {
         setNav(!nav);
     };
 
+
     return (
         <div style={{ backgroundColor: `${color}` }} className={color ? 'fixed w-full h-20  z-[100] ease-in-out duration-300' : 'fixed w-full h-20 z-[100]'}>
             <div className='flex justify-between items-center w-full h-full px-2 2xl:px-16'>
-                <Link href='/'style={{ marginTop: '10px',marginLeft:'10px' }}>
-                    <Image src={sicca} alt='SicaaDev Logo' width={80} height={80} />
-                    {/* <h1 style={{ color: `${textColor}`,marginLeft:'-15px' }} className='font-bold text-3xl'>
-                         SicaaDev
-</h1> */}
+                <Link href='/'>
+                    <div className='flex items-center'>
+                        <Image src={sicca} alt='SicaaDev Logo' width={50}  />
+                        <h1 style={{ color: `${textColor}` }} className='font-bold text-4xl mb-1 ml-2'>
+                            SicaaDev
+                        </h1>
+                    </div>
                 </Link>
 
                 <div>
                     <ul style={{ color: `${textColor}` }} className='hidden md:flex'>
                         {links.map((link) => (
-                            <li key={link.url} className='ml-10 text-sm uppercase transform hover:scale-110 hover:border-b-2 hover:border-red-500 transition duration-400 ease-in-out p-2 rounded-lg hover:bg-white-100 '>
+                            <li key={link.url} className='ml-10 text-sm uppercase hover:border-b'>
                                 <ScrollLink
                                     to={link.url}
                                     smooth={true}
